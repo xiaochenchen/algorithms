eef0c7addc0ba56f2b605dc47f39a61866a979cb
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-06-23 15:52:34 -0400

    add program to calculate primes numbers using Sieve of Eratosthenes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This file was created by IntelliJ IDEA 12.1.4 for binding GitHub repository
+
+Sample programs to study and practise algorithms
+
+(1)Sieve of Eratosthenes
+Time Complexity: n(1/2 + 1/3 + 1/5 + 1/7 + ...) = O(n log log n), there is a sum of  reciprocals of primes up to n involved
---
+++ src/main/java/com/xcc/SieveOfEratosthenes.java
@@ -0,0 +1,55 @@
+package com.xcc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xiaochen Chen
+ * Date: 6/23/13
+ * Time: 2:51 PM
+ */
+public class SieveOfEratosthenes {
+
+    public static void main(String args[])
+    {
+        System.out.println("Please enter the upper limit for Sieve Of Eratothenes:");
+
+        Scanner scanner = new Scanner(System.in);
+        int limit = Integer.parseInt(scanner.next());
+
+        SieveOfEratosthenes soe = new SieveOfEratosthenes();
+        soe.printPrimesLessThan(limit);
+    }
+
+    public void printPrimesLessThan(int limit)
+    {
+        boolean[] isPrime = new boolean[limit + 1]; // boolean array indicates if the index number is a prime
+        Arrays.fill(isPrime, true); // default to true for all numbers
+        isPrime[0] = false; isPrime[1] = false;
+
+        for(int i = 2; i * i < limit + 1; ++i)
+        {
+            if(isPrime[i] == true)
+            {
+                for(int j = i * i; j < limit + 1; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 2; i < limit + 1; ++i)
+        {
+            if(isPrime[i] == true)
+            {
+                sb.append(i).append(',');
+            }
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb.toString());
+    }
+}
